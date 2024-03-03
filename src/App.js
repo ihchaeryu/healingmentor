34ceb06bce25347f574d8fb42e49dd55d8b60e20
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-03 16:16:48 +0900

    INIT) chakra ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme'
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { theme as chakraTheme } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const config = {
+        colors: {
+
+        },
+        fonts: {
+
+        },
+    };
+
+const theme = extendTheme({ config });
+
+export default theme;
