4e645ef0187a54505a67e111c32f1838810503ca
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-04 21:43:18 +0900

    FEAT) home upper main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Center } from "@chakra-ui/react";
+import { Flex, Box, Text, Divider } from "@chakra-ui/react";
 
 const Home = () => {
 
     return (
         <Layout>
-            <Center w='100%' h='100%' bg='gray.100'>
-                Home!
-            </Center>
+            <Flex 
+            direction='column'
+            bg='gray.100'
+            p={5}
+            gap={3}
+            >
+                <Flex
+                direction='column'
+                bg='gray.300'
+                p={2}
+                alignItems='center'
+                >
+                    <Text fontSize='xl' fontWeight='bold' textAlign='center'>
+                        Welcome to Healing Mentor!
+                    </Text>
+                    <Box as="img" src='' alt='main entrance image' w='300px' h='200px' />
+                </Flex>
+                <Divider orientation='horizontal' />
+            </Flex>
         </Layout>
     );
 };
