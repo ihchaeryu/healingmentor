b2cc47ffe2a666b001fc72db40c0ae8e427cf130
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-04 23:11:45 +0900

    trivial commit

--- src/components/SlickSlider.js
+++ src/components/SlickSlider.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { Center, Text, } from '@chakra-ui/react';
+import './SlickSlider.css';
 
 const SlickSlider = () => {
+
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
       };
 
     return (
         <Slider {...settings}>
-            <div>
+            <div className='slide'>
                 <Center bg='green' w='300px' h='120px'>
                     <Text textColor='white'>1</Text>
                 </Center>
             </div>
-            <div>
+            <div className='slide'>
                 <Center bg='purple' w='300px' h='120px'>
                     <Text textColor='white'>2</Text>
                 </Center>
             </div>
-            <div>
+            <div className='slide'>
                 <Center bg='blue' w='300px' h='120px'>
                     <Text textColor='white'>3</Text>
                 </Center>
             </div>
-            <div>
+            <div className='slide'>
                 <Center bg='orange' w='300px' h='120px'>
                     <Text textColor='white'>4</Text>
                 </Center>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Flex, Box, Text, Divider } from "@chakra-ui/react";
+import { Flex, Box, Text, Divider, Image } from "@chakra-ui/react";
 import SlickSlider from "../components/SlickSlider";
+import mainImage from '../assets/healimg.jpeg';
 
 const Home = () => {
 
@@ -15,28 +16,28 @@ const Home = () => {
             >
                 <Flex
                 direction='column'
-                bg='gray.300'
                 p={2}
                 alignItems='center'
+                gap={1}
                 >
                     <Text fontSize='xl' fontWeight='bold' textAlign='center'>
                         Welcome to Healing Mentor!
                     </Text>
-                    <Box as="img" src='' alt='main entrance image' w='300px' h='200px' />
+                    <Image src={mainImage} alt='main entrance image' objectFit='cover' w='300px' h='200px' p={3} />
                 </Flex>
                 
                 <Divider orientation='horizontal' borderColor='gray.600' />
 
                 <Flex
                 direction='column'
-                bg='gray.300'
+                // bg='gray.300'
                 p={2}
                 alignItems='center'
                 >
                     <Text fontSize='lg' fontWeight='bold' textAlign='center'>
                         Testimonial
                     </Text>
-                    <Box w='350px' h='200px' p={5} m={2} bg='gray.400' >
+                    <Box w='350px' h='200px' p={5} m={2} >
                         <SlickSlider />
                     </Box>
                 </Flex>
