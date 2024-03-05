3bad44485c1f0f90fc3e777145a0ead619395a4e
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-06 02:09:53 +0900

    FEAT) meditation page

--- src/pages/Meditation.js
+++ src/pages/Meditation.js
@@ -1,14 +1,67 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Center } from "@chakra-ui/react";
+import { Flex, Text, Image, Spacer, } from "@chakra-ui/react";
+import mainImage from '../assets/healimg.jpeg'; // placeholder
 
 const Meditation = () => {
 
     return (
         <Layout>
-            <Center w='100%' h='100%' bg='gray.100'>
-                Meditation!
-            </Center>
+            <Flex 
+            direction='column'
+            bg='gray.100'
+            p={5}
+            gap={3}
+            >
+                <Flex
+                direction='row'
+                // bg='gray.200'
+                borderWidth={1}
+                borderColor='gray.400'
+                borderRadius={10}
+                p={5}
+                alignItems='center'
+                >
+                    <Text fontSize='lg' fontWeight='bold'>
+                        Guided Meditation
+                    </Text>
+                    <Spacer />
+                    <Image src={mainImage} alt="guided meditation" w='120px' h='120px' objectFit='cover' />
+                </Flex>
+
+                <Flex
+                direction='row'
+                // bg='gray.200'
+                borderWidth={1}
+                borderColor='gray.400'
+                borderRadius={10}
+                p={5}
+                alignItems='center'
+                >
+                    <Text fontSize='lg' fontWeight='bold'>
+                        Respiratory Graph
+                    </Text>
+                    <Spacer />
+                    <Image src={mainImage} alt="respiratory graph" w='120px' h='120px' objectFit='cover' />
+                </Flex>
+
+                <Flex
+                direction='row'
+                // bg='gray.200'
+                borderWidth={1}
+                borderColor='gray.400'
+                borderRadius={10}
+                p={5}
+                alignItems='center'
+                >
+                    <Text fontSize='lg' fontWeight='bold'>
+                        Sustained Attention
+                    </Text>
+                    <Spacer />
+                    <Image src={mainImage} alt="sustained attention" w='120px' h='120px' objectFit='cover' />
+                </Flex>
+
+            </Flex>
         </Layout>
     );
 };
