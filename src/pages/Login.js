70f117eb455530e59ba275c6ebee8ed433217b0c
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-06 12:06:48 +0900

    INIT) chakra icons installed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Center } from "@chakra-ui/react";
+import { Flex, } from "@chakra-ui/react";
 
 const Login = () => {
 
     return (
-        <Layout>
-            <Center w='100%' h='100%' bg='gray.100'>
-                Login!
-            </Center>
-        </Layout>
+        <Flex
+        direction='row'
+        justifyContent='flex-start'
+        >
+            
+        </Flex>
     );
 };
 
