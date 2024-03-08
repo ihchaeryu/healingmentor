42cc2ad5d947472025bb2b4e3d83bdfcdb7d5507
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-08 16:09:30 +0900

    FEAT) med subpages basic layouts

--- src/pages/GuidedMed.js
+++ src/pages/GuidedMed.js
@@ -1,14 +1,38 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Center } from "@chakra-ui/react";
+import { Flex, Text, } from "@chakra-ui/react";
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
 
 const GuidedMed = () => {
 
     return (
         <Layout>
-            <Center w='100%' h='100%' bg='gray.100'>
-                GuidedMed!
-            </Center>
+            <Flex
+            direction='column'
+            p={4}
+            gap={5}
+            >
+                <Flex
+                direction='row'
+                justifyContent='flex-start'
+                alignItems='flex-end'
+                >
+                    <Text fontSize='2xl' fontWeight='bold'>
+                        Guided Meditation
+                    </Text>
+                    <QuestionOutlineIcon boxSize={4} m={2} color='blue' />
+                </Flex>
+
+                <Flex
+                direction='column'
+                borderWidth={1}
+                borderColor='gray.300'
+                borderRadius={10}
+                h='400px'
+                >
+
+                </Flex>
+            </Flex>
         </Layout>
     );
 };
--- src/pages/SustAttention.js
+++ src/pages/SustAttention.js
@@ -1,14 +1,38 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Center } from "@chakra-ui/react";
+import { Flex, Text, } from "@chakra-ui/react";
+import { QuestionOutlineIcon } from "@chakra-ui/icons";
 
 const SustAttention = () => {
 
     return (
         <Layout>
-            <Center w='100%' h='100%' bg='gray.100'>
-                SustAttention!
-            </Center>
+            <Flex
+            direction='column'
+            p={4}
+            gap={5}
+            >
+                <Flex
+                direction='row'
+                justifyContent='flex-start'
+                alignItems='flex-end'
+                >
+                    <Text fontSize='2xl' fontWeight='bold'>
+                        Sustained Attention
+                    </Text>
+                    <QuestionOutlineIcon boxSize={4} m={2} color='blue' />
+                </Flex>
+
+                <Flex
+                direction='column'
+                borderWidth={1}
+                borderColor='gray.300'
+                borderRadius={10}
+                h='400px'
+                >
+
+                </Flex>
+            </Flex>
         </Layout>
     );
 };
