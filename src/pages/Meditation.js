0ff0a0848ea1fc8c1fc595ea8bc3ba20e81b5852
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-08 12:36:35 +0900

    FEAT) med page sub links

--- src/pages/Meditation.js
+++ src/pages/Meditation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { Flex, Text, Image, Spacer, } from "@chakra-ui/react";
+import { Flex, Text, Image, Spacer, Link, } from "@chakra-ui/react";
+import { Link as RouterLink } from 'react-router-dom';
 import mainImage from '../assets/healimg.jpeg'; // placeholder
 
 const Meditation = () => {
@@ -12,53 +13,56 @@ const Meditation = () => {
             p={5}
             gap={3}
             >
-                <Flex
-                direction='row'
-                // bg='gray.200'
-                borderWidth={1}
-                borderColor='gray.400'
-                borderRadius={10}
-                p={5}
-                alignItems='center'
-                >
-                    <Text fontSize='lg' fontWeight='bold'>
-                        Guided Meditation
-                    </Text>
-                    <Spacer />
-                    <Image src={mainImage} alt="guided meditation" w='120px' h='120px' objectFit='cover' />
-                </Flex>
+                <Link as={RouterLink} to='/meditation/gm'>
+                    <Flex
+                    direction='row'
+                    borderWidth={1}
+                    borderColor='gray.400'
+                    borderRadius={10}
+                    p={5}
+                    alignItems='center'
+                    >
+                        <Text fontSize='lg' fontWeight='bold'>
+                            Guided Meditation
+                        </Text>
+                        <Spacer />
+                        <Image src={mainImage} alt="guided meditation" w='120px' h='120px' objectFit='cover' />
+                    </Flex>
+                </Link>
 
-                <Flex
-                direction='row'
-                // bg='gray.200'
-                borderWidth={1}
-                borderColor='gray.400'
-                borderRadius={10}
-                p={5}
-                alignItems='center'
-                >
-                    <Text fontSize='lg' fontWeight='bold'>
-                        Respiratory Graph
-                    </Text>
-                    <Spacer />
-                    <Image src={mainImage} alt="respiratory graph" w='120px' h='120px' objectFit='cover' />
-                </Flex>
+                <Link as={RouterLink} to='/meditation/rg'>
+                    <Flex
+                    direction='row'
+                    borderWidth={1}
+                    borderColor='gray.400'
+                    borderRadius={10}
+                    p={5}
+                    alignItems='center'
+                    >
+                        <Text fontSize='lg' fontWeight='bold'>
+                            Respiratory Graph
+                        </Text>
+                        <Spacer />
+                        <Image src={mainImage} alt="respiratory graph" w='120px' h='120px' objectFit='cover' />
+                    </Flex>
+                </Link>
 
-                <Flex
-                direction='row'
-                // bg='gray.200'
-                borderWidth={1}
-                borderColor='gray.400'
-                borderRadius={10}
-                p={5}
-                alignItems='center'
-                >
-                    <Text fontSize='lg' fontWeight='bold'>
-                        Sustained Attention
-                    </Text>
-                    <Spacer />
-                    <Image src={mainImage} alt="sustained attention" w='120px' h='120px' objectFit='cover' />
-                </Flex>
+                <Link as={RouterLink} to='/meditation/sa'>
+                    <Flex
+                    direction='row'
+                    borderWidth={1}
+                    borderColor='gray.400'
+                    borderRadius={10}
+                    p={5}
+                    alignItems='center'
+                    >
+                        <Text fontSize='lg' fontWeight='bold'>
+                            Sustained Attention
+                        </Text>
+                        <Spacer />
+                        <Image src={mainImage} alt="sustained attention" w='120px' h='120px' objectFit='cover' />
+                    </Flex>
+                </Link>
 
             </Flex>
         </Layout>
