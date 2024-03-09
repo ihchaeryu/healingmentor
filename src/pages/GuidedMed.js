cb9483b6c5774445d80b0c4db2956a815d2279b5
ryuihchae <ihchaeryu@kaist.ac.kr>
2024-03-10 01:55:48 +0900

    FEAT) guidedmed page

--- src/pages/GuidedMed.js
+++ src/pages/GuidedMed.js
@@ -1,9 +1,36 @@
-import React from "react";
+import { React, useState } from "react";
 import Layout from "../components/Layout";
-import { Flex, Text, } from "@chakra-ui/react";
-import { QuestionOutlineIcon } from "@chakra-ui/icons";
+import { Flex, Text, Button, Progress, } from "@chakra-ui/react";
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+  } from '@chakra-ui/react'
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+  } from '@chakra-ui/react'
+import { QuestionOutlineIcon, ChevronDownIcon } from "@chakra-ui/icons";
 
 const GuidedMed = () => {
+
+    const [lecture, setLecture] = useState(0);
+    const lectures = ["Select Lecture to Listen", "1. Body Scan", "2. Respiratory Meditation", "3. Mindfulness Meditation"];
+    const handleLectureClick = (n) => {
+        setLecture(n);
+    };
 
     return (
         <Layout>
@@ -20,7 +47,17 @@ const GuidedMed = () => {
                     <Text fontSize='2xl' fontWeight='bold'>
                         Guided Meditation
                     </Text>
-                    <QuestionOutlineIcon boxSize={4} m={2} color='blue' />
+                    <Popover>
+                        <PopoverTrigger>
+                            <QuestionOutlineIcon as='Button' boxSize={4} m={2} color='blue' />
+                        </PopoverTrigger>
+                        <PopoverContent boxShadow='2xl'>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverHeader>Instructions</PopoverHeader>
+                            <PopoverBody>heyy</PopoverBody>
+                        </PopoverContent>
+                    </Popover>
                 </Flex>
 
                 <Flex
@@ -28,9 +65,33 @@ const GuidedMed = () => {
                 borderWidth={1}
                 borderColor='gray.300'
                 borderRadius={10}
-                h='400px'
+                justifyContent='center'
+                p={5}
+                gap={3}
                 >
-
+                    <Flex
+                    direction='row'
+                    justifyContent='space-between'
+                    >
+                        <Menu>
+                            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} fontWeight='regular' variant='outline' justifySelf='stretch'>
+                                {lectures[lecture]}
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem onClick={() => handleLectureClick(0)}>Select Lecture to Listen</MenuItem>
+                                <MenuItem onClick={() => handleLectureClick(1)}>1. Body Scan</MenuItem>
+                                <MenuItem onClick={() => handleLectureClick(2)}>2. Respiratory Meditation</MenuItem>
+                                <MenuItem onClick={() => handleLectureClick(3)}>3. Mindfulness Meditation</MenuItem>
+                            </MenuList>
+                        </Menu>
+                        <Button variant='outline'>
+                            Play
+                        </Button>
+                    </Flex>
+                    <Progress value={30} />
+                    <Button justifySelf='stretch' colorScheme='blue'>
+                        Write meditation report
+                    </Button>
                 </Flex>
             </Flex>
         </Layout>
